d71d9d01d4cf1757b52c2c8784fd3cc0cfe72f29
DESKTOP-8E7O7UO\Siwonschool <jiwoo@siwonschool.com>
2026-01-15 09:37:37 +0900

    feat: ui작업업

---
+++ script.js
@@ -0,0 +1,29 @@
+// const input = document.querySelector('#todoInput');
+// const addBtn = document.querySelector('#addBtn');
+// const list = document.querySelector('#todoList');
+
+// addBtn.addEventListener('click', () => {
+//   const text = input.value;
+//   if (text === '') return;
+
+//   const li = document.createElement('li');
+//   li.textContent = text;
+
+//   li.addEventListener('click', () => {
+//     li.classList.toggle('done');
+//   });
+
+//   const deleteBtn = document.createElement('button');
+//   deleteBtn.textContent = '삭제';
+
+//   deleteBtn.addEventListener('click', () => {
+//     li.remove();
+//   });
+
+//   li.appendChild(deleteBtn);
+//   list.appendChild(li);
+
+//   input.value = '';
+// });
+
+// console.log("JS 연결됨!");
